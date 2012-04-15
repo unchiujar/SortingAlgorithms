e033a5c9195b3e3056f635825e1f676accd1afbd
Vasile Jureschi <vasile.jureschi@gmail.com>
2012-04-15 03:14:30 +0300

    Added benchmark for bubble sort

--- src/test/java/org/unchiujar/algorithms/sorting/test/RunAlgorithms.java
+++ src/test/java/org/unchiujar/algorithms/sorting/test/RunAlgorithms.java
@@ -9,12 +9,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.unchiujar.algorithms.sorting.BubbleSort;
 
+import com.google.caliper.Param;
 import com.google.caliper.Runner;
 import com.google.caliper.SimpleBenchmark;
 
 public class RunAlgorithms extends SimpleBenchmark {
-    private static final Logger LOG = LoggerFactory
-                                            .getLogger(RunAlgorithms.class);
+    private static final Logger            LOG =
+                                                       LoggerFactory
+                                                               .getLogger(RunAlgorithms.class);
+
+    @Param({ "10", "100", "1000", "10000" })
+    private int                            length;
+    @Param
+    private Distribution                   distribution;
+
+    private LinkedHashMap<Integer, String> values;
+    private LinkedHashMap<Integer, String> copy;
+
+    private BubbleSort<Integer, String> bubble = new BubbleSort<Integer, String>();
+
+    @Override
+    protected void setUp() throws Exception {
+        LOG.debug("Setting up with length : {} and distribution {} ", length,  distribution);
+        values = distribution.generateData(length);
+        copy = new LinkedHashMap<Integer, String>(length);
+    }
 
     @Test
     public void bubbleSort() throws Exception {
@@ -22,27 +41,70 @@ public class RunAlgorithms extends SimpleBenchmark {
     }
 
     public void timeBubbleSort(int reps) throws Exception {
-        BubbleSort<Integer, String> bubble = new BubbleSort<Integer, String>();
+        copy.putAll(values);
         for (int i = 0; i < reps; i++) {
-            LinkedHashMap<Integer, String> data = generateData(10000);
-            LOG.debug("The sorted list is : {}", bubble.sort(data));
+             bubble.sort(copy);
         }
     }
 
-    
-    public void timeGenerateData(int reps){
-        for (int i = 0; i < reps; i++) {
-            LinkedHashMap<Integer, String> data = generateData(10000);
-        }
-        
-    }
-    
-    private LinkedHashMap<Integer, String> generateData(int size) {
-        LinkedHashMap<Integer, String> data =
-                new LinkedHashMap<Integer, String>();
-        for (int i = 0; i < size; i++) {
-            data.put(new Random().nextInt(), UUID.randomUUID().toString());
-        }
-        return data;
+    public enum Distribution {
+        SAWTOOTH {
+            @Override
+            LinkedHashMap<Integer, String> generateData(int length) {
+                LinkedHashMap<Integer, String> data =
+                        new LinkedHashMap<Integer, String>(length);
+
+                for (int i = 0; i < length; i += 5) {
+                    data.put(i, UUID.randomUUID().toString());
+                    data.put(i + 1, UUID.randomUUID().toString());
+                    data.put(i + 2, UUID.randomUUID().toString());
+                    data.put(i + 3, UUID.randomUUID().toString());
+                    data.put(i + 4, UUID.randomUUID().toString());
+                }
+                return data;
+            }
+        },
+        INCREASING {
+
+            @Override
+            LinkedHashMap<Integer, String> generateData(int length) {
+                LinkedHashMap<Integer, String> data =
+                        new LinkedHashMap<Integer, String>(length);
+
+                for (int i = 0; i < length; i++) {
+                    data.put(i, UUID.randomUUID().toString());
+                }
+                return data;
+
+            }
+
+        },
+        DECREASING {
+            @Override
+            LinkedHashMap<Integer, String> generateData(int length) {
+                LinkedHashMap<Integer, String> data =
+                        new LinkedHashMap<Integer, String>(length);
+
+                for (int i = length; i > 0; i--) {
+                    data.put(i, UUID.randomUUID().toString());
+                }
+                return data;
+            }
+
+        },
+        RANDOM {
+            @Override
+            LinkedHashMap<Integer, String> generateData(int length) {
+                LinkedHashMap<Integer, String> data =
+                        new LinkedHashMap<Integer, String>(length);
+                for (int i = 0; i < length; i++) {
+                    data.put(new Random().nextInt(), UUID.randomUUID()
+                            .toString());
+                }
+                return data;
+            }
+        };
+
+        abstract LinkedHashMap<Integer, String> generateData(int length);
     }
 }
